c859a2750156010daceb47d98c4e233d2567c5f0
christianmat <christian_mathiesen@alumni.brown.edu>
2024-01-07 21:51:50 -0800

    update interface to match latest spec

--- packages/js-client/src/core/remote-storage.ts
+++ packages/js-client/src/core/remote-storage.ts
@@ -1,45 +1,47 @@
 import fetch from 'cross-fetch'
+import { uuid } from './utils'
+
+const apiPrefix = `/entities/`
+
+interface RemoteStorageProps {
+  serverAddress: string
+  instanceId?: string
+  userId?: string
+}
 
 export class RemoteStorage {
   private readonly serverAddress: string
   private readonly instanceId: string
   private readonly userId: string
 
-  constructor(serverAddress: string, instanceId: string, userId: string) {
+  constructor({ serverAddress, instanceId, userId }: RemoteStorageProps) {
     this.serverAddress = serverAddress
-    this.instanceId = instanceId
-    this.userId = userId
+    this.instanceId = instanceId ?? uuid()
+    this.userId = userId ?? uuid()
   }
 
   async getItem<T>(key: string): Promise<T> {
-    const response = await this.call('GET', `/api/entity/${key}`, null)
+    const response = await this.call('GET', `${apiPrefix}${key}`, null)
     return await response.json()
   }
 
   async setItem<T>(key: string, value: T): Promise<void> {
-    await this.call('PUT', `/api/entity/${key}`, value)
+    await this.call('PUT', `${apiPrefix}${key}`, value)
   }
 
   async removeItem(key: string): Promise<void> {
-    await this.call('DELETE', `/api/entity/${key}`, null)
+    await this.call('DELETE', `${apiPrefix}${key}`, null)
   }
 
-  async call(method: string, path: string, data: any) {
-    // Throw error if not initialized
-    if (!this.serverAddress || !this.instanceId || !this.userId) {
-      throw new Error(
-        'remoteStorage has not been initialized. Please call remoteStorage.init() first.'
-      )
-    }
-
-    return fetch(`${this.serverAddress}${path}`, {
+  async call(method: string, path: string, data?: any) {
+    return fetch(new URL(path, this.serverAddress).toString(), {
       method: method,
       headers: {
         'Content-Type': 'application/json',
         HEADER_REMOTE_STORAGE_INSTANCE_ID: this.instanceId,
         HEADER_REMOTE_STORAGE_USER_ID: this.userId,
       },
-      body: JSON.stringify(data),
+      body: data ? JSON.stringify(data) : undefined,
     })
   }
 }
