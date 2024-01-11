0c8b65d9d8e4e503eb417d5d71134ea80bab1b2c
christianmat <christian_mathiesen@alumni.brown.edu>
2024-01-11 14:55:04 -0800

    add posthog

--- apps/www/pages/_app.tsx
+++ apps/www/pages/_app.tsx
@@ -1,9 +1,10 @@
 import 'styles/globals.scss'
 
-import type { AppProps } from 'next/app'
-import { ThemeProvider } from 'next-themes'
-import { NextSeo } from 'next-seo'
+import type {AppProps} from 'next/app'
+import {ThemeProvider} from 'next-themes'
+import {NextSeo} from 'next-seo'
 import Head from 'next/head'
+import Script from "next/script";
 
 const title = 'remoteStorage'
 const description = 'Easily persist data across browsers and devices.'
@@ -33,6 +34,10 @@ export default function App({ Component, pageProps }: AppProps) {
           ],
         }}
       />
+      <Script id="posthog">
+        {`!function(t,e){var o,n,p,r;e.__SV||(window.posthog=e,e._i=[],e.init=function(i,s,a){function g(t,e){var o=e.split(".");2==o.length&&(t=t[o[0]],e=o[1]),t[e]=function(){t.push([e].concat(Array.prototype.slice.call(arguments,0)))}}(p=t.createElement("script")).type="text/javascript",p.async=!0,p.src=s.api_host+"/static/array.js",(r=t.getElementsByTagName("script")[0]).parentNode.insertBefore(p,r);var u=e;for(void 0!==a?u=e[a]=[]:a="posthog",u.people=u.people||[],u.toString=function(t){var e="posthog";return"posthog"!==a&&(e+="."+a),t||(e+=" (stub)"),e},u.people.toString=function(){return u.toString(1)+".people (stub)"},o="capture identify alias people.set people.set_once set_config register register_once unregister opt_out_capturing has_opted_out_capturing opt_in_capturing reset isFeatureEnabled onFeatureFlags getFeatureFlag getFeatureFlagPayload reloadFeatureFlags group updateEarlyAccessFeatureEnrollment getEarlyAccessFeatures getActiveMatchingSurveys getSurveys onSessionId".split(" "),n=0;n<o.length;n++)g(u,o[n]);e._i.push([i,s,a])},e.__SV=1)}(document,window.posthog||[]);
+          posthog.init('phc_N7OxUp5H8Tt5zZyaYevvnr748X4VserVtAvUBk0Ve17',{api_host:'https://app.posthog.com'})`}
+      </Script>
       <ThemeProvider disableTransitionOnChange attribute="class">
         <Component {...pageProps} />
       </ThemeProvider>
