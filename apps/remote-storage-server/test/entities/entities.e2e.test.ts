7b5dde2d04133f9d735267e6c4bf542c3973b6bb
christianmat <christian_mathiesen@alumni.brown.edu>
2024-01-21 22:01:15 -0800

    fix(tests): add missing modules to unit test

--- apps/remote-storage-server/test/entities/entities.e2e.test.ts
+++ apps/remote-storage-server/test/entities/entities.e2e.test.ts
@@ -1,9 +1,12 @@
 import * as request from 'supertest'
-import {Test, TestingModule} from '@nestjs/testing'
-import {INestApplication} from '@nestjs/common'
-import {EntitiesModule} from '../../src/entities/entities.module'
-import {AppModule} from '../../src/app.module'
-import {RedisService} from '../../src/services/data/redis/redis.service'
+import { Test, TestingModule } from '@nestjs/testing'
+import { INestApplication } from '@nestjs/common'
+import { EntitiesModule } from '../../src/entities/entities.module'
+import { AppModule } from '../../src/app.module'
+import { RedisService } from '../../src/services/data/redis/redis.service'
+import { DataServiceModule } from '../../src/services/data/data-service/data-service.module'
+import { RedisModule } from '../../src/services/data/redis/redis.module'
+import { SqliteModule } from '../../src/services/data/sqlite/sqlite.module'
 
 describe('entities/', () => {
   let app: INestApplication
@@ -14,7 +17,7 @@ describe('entities/', () => {
     beforeAll(async () => {
       const redisService = new RedisService()
       const testModule: TestingModule = await Test.createTestingModule({
-        imports: [EntitiesModule, AppModule],
+        imports: [EntitiesModule, DataServiceModule, RedisModule, SqliteModule, AppModule],
       })
         .overrideProvider(RedisService)
         .useValue(redisService)
